67a413cfe492f052c8d9cfec6bc596610e96962a
pogkythunder <82233102+pogkythunder@users.noreply.github.com>
2021-05-11 12:33:42 +0000

    Add files via upload

---
+++ afiloxenos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# shellcheck disable=SC1091
+
+source "$HOME"/.afiloxenos/lang.sh
+
+function box_out()
+{
+	printf " %s " "$(tput setaf 6)${arr[RANDOM%${#arr[@]}+0]}$(tput sgr0)"| boxes -d stone -p a2v1
+}
+
+box_out "${arr[@]}"
+echo -e "\n"
+
